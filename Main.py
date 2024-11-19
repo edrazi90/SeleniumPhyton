55e200b42c9817d0b8a31890152e2c5531985a44
edrazi90 <edrazi90@gmail.com>
2024-11-20 01:03:30 +0800

    commit

--- Main.py
+++ Main.py
@@ -8,13 +8,47 @@ import time
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
-
+#Navigate to URL
 driver.get("https://www.airasia.com/en/gb")
 
-time.sleep(10)
-
-WebDriverWait(driver, 5).until(
+#wait til page is displayed
+WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID,"origin"))
 )
 
-input_element = driver.get_element(By.ID,"origin")
+
+#Initialize attributes
+tripType = driver.find_element(By.ID,"home_triptype")
+origin = driver.find_element(By.ID,"origin")
+destination = driver.find_element(By.ID,"destination")
+depart_date = driver.find_element(By.ID,"departclick-handle")
+return_date = driver.find_element(By.ID,"returnclick-handle")
+
+
+
+#Select trip type
+
+
+
+
+
+
+
+#Input origin 
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable(origin))  # Ensure the element is clickable
+origin.click()
+origin.send_keys("Kuala Lumpur")
+WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'suggestions')]")))  # Wait for suggestions to appear
+origin.send_keys(Keys.ENTER)
+
+#Input destination
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable(destination))
+origin.click()
+origin.send_keys("Changi Airport")
+WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'suggestions')]")))  # Wait for suggestions to appear
+origin.send_keys(Keys.ENTER)
+
+
+
+
+driver.quit()
