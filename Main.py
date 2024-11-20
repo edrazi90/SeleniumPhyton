1765651891e0555dd0f5bf3439c9045bcd687261
edrazi90 <edrazi90@gmail.com>
2024-11-20 16:24:48 +0800

    commit

--- Main.py
+++ Main.py
@@ -5,76 +5,143 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementNotInteractableException, StaleElementReferenceException, WebDriverException
+from selenium.webdriver.chrome.options import Options
 import time
 
 
-service = Service(executable_path="chromedriver.exe")
+
+
+#Initialize driver
+chrome_options = Options()
+service = Service(executable_path="chromedriver.exe",Options=chrome_options)
 driver = webdriver.Chrome(service=service)
+chrome_options.add_experimental_option("detach", True)
 
 
 
-    
+
+    #Initialize data parameter
+userName = "user@yahoo.com.my"
+password = "password121"
+home = "Johor Baharu"
+travelDestination = "Singapore"
+departDate = "2024-12-15" 
+returnDate = "2024-12-20"
+passengerTotal = "2"
+
+
+
 #Navigate to URL
 driver.get("https://www.airasia.com/en/gb")
 driver.maximize_window()
 
-#wait til page is displayed
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID,"origin"))
+#Initialize input parameter needed for homepage
+login = driver.find_element(By.ID,"login-component")
+tripType = driver.find_element(By.ID,"home_triptype")
+paxSection = driver.find_element(By.ID,"airasia-dropdown-options-title")
+origin = driver.find_element(By.ID,"flight-place-picker")
+destination = driver.find_element(By.ID,"home_flyingfrom")
+depart_date = driver.find_element(By.ID,"departclick-handle")
+return_date = driver.find_element(By.ID,"returnclick-handle")
+search_button = driver.find_element(By.ID,"home_Search")
+
+
+#wait til HomePage is displayed
+WebDriverWait(driver, 15).until(
+    EC.presence_of_element_located((By.ID,"login-component"))
 )
 
 
-#Initialize attributes
-tripType = driver.find_element(By.ID,"home_triptype")
-paxSection = driver.find_element(By.ID,"airasia-dropdown-options-title")
-origin = driver.find_element(By.ID,"origin")
-destination = driver.find_element(By.ID,"destination")
-depart_date = driver.find_element(By.ID,"departclick-handle")
-return_date = driver.find_element(By.ID,"returnclick-handle")
 
+"""
+#Press login section and login to the system
+login.click()
+time.sleep(5)
 
+#Login banner paremeter
+mailTextbox = driver.find_element(By.ID,"text-input--login")
+passwordTextbox =driver.find_element(By.ID,"password-input--login")
+loginButton = driver.find_element(By.ID,"loginbutton")
 
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable(mailTextbox))
+mailTextbox.send_keys(userName)
+time.sleep(5)
+passwordTextbox.send_keys(password)
+time.sleep(5)
+loginButton.click()
+time.sleep(5)
+"""
+
+"""
 #Select trip type
 WebDriverWait(driver, 5).until(EC.element_to_be_clickable(tripType))
 tripType.click()
-WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID,"home_oneway")))
-oneWay = driver.find_element(By.ID,"home_oneway")#declare trip type with ID here
-WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID,"home_oneway")))
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,"TripTypes__Label-sc-1xgnhxw-2 hYKgAZ")))
+oneWay = driver.find_element(By.CLASS_NAME,"TripTypes__Label-sc-1xgnhxw-2 hYKgAZ")#declare trip type with ID here
 oneWay.click()
-WebDriverWait(driver,5)
-
+WebDriverWait(driver,10)
+"""
+"""
 #Set pax and travel class
 paxSection.click()
 WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID,"home_premium_economy")))
 premiumEcon = driver.find_element(By.ID,"home_premium_economy")#Initialize Travelclass
 premiumEcon.click()
+"""
 
-
-
+"""
 #clear origin & desination  textbox
 origin.sendKeys(Keys.CONTROL + "a")
 origin.sendKeys(Keys.DELETE)
 
 destination.sendKeys(Keys.CONTROL + "a")
 destination.sendKeys(Keys.DELETE)
-
+"""
 
 #Input origin 
 WebDriverWait(driver, 5).until(EC.element_to_be_clickable(origin))  # Ensure the element is clickable
 origin.click()
-origin.send_keys("Kuala Lumpur")
-WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'suggestions')]")))  # Wait for suggestions to appear
+time.sleep(5)
+
+promoBanner = driver.find_element(By.ID,"wzrk-cancel")#Click cancel when promo banner appeared
+promoBanner.click()
+time.sleep(5)
+
+origin.click()
+time.sleep(5)
+origin.clear()
+origin.send_keys(home)
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'suggestions')]")))  # Wait for suggestions to appear
 origin.send_keys(Keys.ENTER)
 
 #Input destination
 WebDriverWait(driver, 5).until(EC.element_to_be_clickable(destination))
-origin.click()
-origin.send_keys("Changi Airport")
-WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'suggestions')]")))  # Wait for suggestions to appear
-origin.send_keys(Keys.ENTER)
+destination.click()
+time.sleep(5)
+destination.clear()
+destination.send_keys(travelDestination)
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'suggestions')]")))  # Wait for suggestions to appear
+destination.send_keys(Keys.ENTER)
 
-#Input depart date
+#Input depart and destination date 
 WebDriverWait(driver, 5).until(EC.element_to_be_clickable(depart_date))
-depart_date.send_keys("1/12/2024")
+depart_date.send_keys(departDate)
+time.sleep(5)
+WebDriverWait(driver, 5).until(EC.element_to_be_clickable(return_date))
+return_date.send_keys(returnDate)
+time.sleep(5)
+
+
+#Input passenger
+passenger_input = driver.find_element(By.ID, "flight-pax-adult")
+passenger_input.clear()
+passenger_input.send_keys(passengerTotal) 
+passenger_input.send_keys(Keys.ENTER)
+
+#Click search button
+search_button.click()
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "flight-results")))
+
+webdriver.s
 
 
