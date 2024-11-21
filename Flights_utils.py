1c92a07b72c1318f53d2b916c4adfb7e2baa757f
edrazi90 <edrazi90@gmail.com>
2024-11-22 01:30:30 +0800

    commit

--- Flights_utils.py
+++ Flights_utils.py
@@ -1,3 +1,5 @@
+
+"""
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -6,21 +8,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementNotInteractableException, StaleElementReferenceException, WebDriverException
 from selenium.webdriver.chrome.options import Options
-import time
-
-#Initialize driver
-chrome_options = Options()
-service = Service(executable_path="chromedriver.exe",Options=chrome_options)
-driver = webdriver.Chrome(service=service)
-chrome_options.add_experimental_option("detach", True)
+import undetected_chromedriver as UC
+"""
 
 
-#wait til HomePage is displayed
-WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located((By.CLASS_NAME,"multiselect AirportPicker-from multiselect-main AirportPicker-from")))
-
-
- #Initialize data parameter
+"""Initialize data parameter
 SiteURL = "https://www.malaysiaairlines.com/my/en/home.html"
 userName = "user@yahoo.com.my"
 password = "password121"
@@ -29,16 +21,18 @@ travelDestination = "Kuala Lumpur"
 departDate = "2024-01-12"#YYYYDDMM 
 returnDate = "2024-03-12"#YYYYDDMM 
 passengerTotal = "2"
+"""
 
-"""MY"""
+"""MY
 basicDomestic = driver.find_element(By.PARTIAL_LINK_TEXT,"../../../../etiqa/mag/my/doc/MH_Basic_Travel_Protection_MY_Domestic.pdf")
 BasicInternational= driver.find_element(By.PARTIAL_LINK_TEXT,"../../../../etiqa/mag/my/doc/MH_Basic_Travel_Protection_MY_International.pdf")
 internationalTravel= driver.find_element(By.PARTIAL_LINK_TEXT,"../../../../etiqa/mag/my/doc/MH_GOM_Travel Protection_MY_International_PW.pdf")
-"""SG"""
+"""
+"""SG
 basicIntlSG = driver.find_element(By.PARTIAL_LINK_TEXT,"../../../../msig/mag/sg/doc/insurance/Policy_Wording_bundle.pdf")
+"""
 
-
-#Capture all records in the list
+"""capture all records in the list
 records = driver.find_element(By.TAG_NAME,"refx-upsell-premium-row-pres")
 economyClass = driver.find_elements(By.CLASS_NAME,"mat-mdc-button-ripple")
 
@@ -46,3 +40,30 @@ for selectRecord in records:
     if economyClass in records.is_displayed():
         selectRecord = driver.find_element(By.XPATH,"//mat-accordion[@class='ng-star-inserted']/refx-upsell-premium-row-pres[0]")
         selectRecord.click()
+"""
+
+
+"""Verify if expansion panel  and enomony Flex card is displayed
+expPanel= driver.find_element(By.TAG_NAME,"mh-carousel")
+economyFlex = driver.find_element(By.CLASS_NAME,"refx-price-card fare-family-ECOFLXXYMY cabin-eco ng-star-inserted")
+
+for selectCard in expPanel:
+    if economyFlex in expPanel.is_displayed():
+        economyFlex.screenshot('/Screenshots/EconomyFlex.png')
+"""
+
+
+
+    
+"""Verify if Basic Travel Insurance is listed in the selected card list
+list = driver.find_element(By.TAG_NAME,"li")
+basicTravel = driver.find_elements(By.ID,"REFX-FARE-FAMILY.ALL.POLICY-BENEFIT") 
+
+for verifyLink in list:
+    if basicTravel in list.is_displayed():
+        basicTravelLink = driver.find_element(By.PARTIAL_LINK_TEXT,'https://static.prod-eu.wl.ancileo.com/anc/mag/all/doc/index.html')
+        basicTravelLink.click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "understand-cover-main")))
+"""
+
+
--- Main.py
+++ Main.py
@@ -7,12 +7,41 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementNotInteractableException, StaleElementReferenceException, WebDriverException
 from selenium.webdriver.chrome.options import Options
 import time
-from Flights_utils import driver,SiteURL,departDate,returnDate,home,travelDestination,passengerTotal,selectRecord
-from Flights_utils import webdriver,WebDriverWait,EC,By,time
-from Flights_utils import basicDomestic,BasicInternational,internationalTravel,basicIntlSG,records,economyClass
+#from Flights_utils import driver,departDate,returnDate,home,travelDestination,passengerTotal,selectRecord
+#from Flights_utils import basicDomestic,BasicInternational,internationalTravel,basicIntlSG,verifyLink,selectCard
+
+chrome_options = Options()
+service = Service(executable_path="chromedriver.exe",Options=chrome_options)
+driver = webdriver.Chrome(service=service)
+
+#wait til HomePage is displayed
+WebDriverWait(driver, 30).until(
+    EC.presence_of_element_located((By.CLASS_NAME,"multiselect AirportPicker-from multiselect-main AirportPicker-from")))
+
+
+#Initialize data parameter
+SiteURL = "https://www.malaysiaairlines.com/my/en/home.html"
+userName = "user@yahoo.com.my"
+password = "password121"
+home = "Alor Setar"
+travelDestination = "Kuala Lumpur"
+departDate = "2024-01-12"#YYYYDDMM 
+returnDate = "2024-03-12"#YYYYDDMM 
+passengerTotal = "2"
+
+#Basic insurance handbook
+"""MY"""
+basicDomestic = driver.find_element(By.PARTIAL_LINK_TEXT,"../../../../etiqa/mag/my/doc/MH_Basic_Travel_Protection_MY_Domestic.pdf")
+BasicInternational= driver.find_element(By.PARTIAL_LINK_TEXT,"../../../../etiqa/mag/my/doc/MH_Basic_Travel_Protection_MY_International.pdf")
+internationalTravel= driver.find_element(By.PARTIAL_LINK_TEXT,"../../../../etiqa/mag/my/doc/MH_GOM_Travel Protection_MY_International_PW.pdf")
+
+"""SG"""
+basicIntlSG = driver.find_element(By.PARTIAL_LINK_TEXT,"../../../../msig/mag/sg/doc/insurance/Policy_Wording_bundle.pdf")
+
+
 
 #Navigate to URL
-driver.get(SiteURL)
+driver.get("https://www.malaysiaairlines.com/my/en/home.html")
 driver.maximize_window()
 
 #wait til HomePage is displayed
@@ -99,22 +128,34 @@ WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "
 departure_time = driver.find_elements(By.CLASS_NAME,"upsell-premium-row-pres-container panel-show")
 
 """Select record with Economy Class if first record are visible"""
+records = driver.find_element(By.TAG_NAME,"refx-upsell-premium-row-pres")
+economyClass = driver.find_elements(By.CLASS_NAME,"mat-mdc-button-ripple")
 
-selectRecord()
+for selectRecord in records:
+    if economyClass in records.is_displayed():
+        selectRecord = driver.find_element(By.XPATH,"//mat-accordion[@class='ng-star-inserted']/refx-upsell-premium-row-pres[0]")
+        selectRecord.click()
 
-    
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "mat-expansion-panel expanded-section ng-tns-c857250080-10 mat-expanded ng-star-inserted")))
 
-"""Verify if expansion panel  and link is displayed"""
+"""Verify if expansion panel  and enomony Flex card is displayed"""
+expPanel= driver.find_element(By.TAG_NAME,"mh-carousel")
+economyFlex = driver.find_element(By.CLASS_NAME,"refx-price-card fare-family-ECOFLXXYMY cabin-eco ng-star-inserted")
 
-expPanel= driver.find_elements(By.CLASS_NAME,"mat-expansion-panel expanded-section ng-tns-c857250080-10 mat-expanded ng-star-inserted")
-economyFlex = driver.find_elements(By.CLASS_NAME,"fare-card-list-item custom-color ng-star-inserted")
-basicTravel = driver.find_elements(By.CLASS_NAME,"characteristic-content") 
-basicTravelLink = driver.find_element(By.PARTIAL_LINK_TEXT,'https://static.prod-eu.wl.ancileo.com/anc/mag/all/doc/index.html')
+for selectCard in expPanel:
+    if economyFlex in expPanel.is_displayed():
+        economyFlex.screenshot('/Screenshots/EconomyFlex.png')
 
-basicTravelLink.click()
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "understand-cover-main")))
 
+"""Verify if Basic Travel Insurance is listed in the selected card list"""
+list = driver.find_element(By.TAG_NAME,"li")
+basicTravel = driver.find_elements(By.ID,"REFX-FARE-FAMILY.ALL.POLICY-BENEFIT") 
+
+for verifyLink in list:
+    if basicTravel in list.is_displayed():
+        basicTravelLink = driver.find_element(By.PARTIAL_LINK_TEXT,'https://static.prod-eu.wl.ancileo.com/anc/mag/all/doc/index.html')
+        basicTravelLink.click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "understand-cover-main")))
 
 
 """Verify all basic Travel TnC document are displayed"""
